c1c0db9c90b7a0d5df4549c3094f8e3d283e21df
phillipchan2 <phillip.chan@liferay.com>
2018-08-19 16:59:23 -0500

    Mock AuthService

--- src/services/authService/authService.js
+++ src/services/authService/authService.js
@@ -1,4 +1,23 @@
 class AuthService {
+    static fakeUser = {
+        description: 'Pellentesque a mi vel tellus venenatis accumsan. Aliquam ultrices magna bibendum enim sagittis, eget commodo turpis suscipit. Maecenas ut ex mattis, accumsan nibh quis, congue turpis. Fusce eu tellus ac erat sagittis aliquet. Sed euismod justo et consequat tempus.',
+        id: 23,
+        name: 'Phil',
+        profilePicture: '',
+    }
+
+    static login() {
+
+    }
+
+    static logout() {
+
+    }
+
+    static getCurentUser() {
+        return this.fakeUser;
+    }
+
     loggedIn = true;
 }
 
