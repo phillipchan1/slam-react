00ab825c85b24d5c4ecc8f0eb31ffeeafb8e3e28
phillipchan2 <phillip.chan@liferay.com>
2018-09-05 11:53:00 -0700

    Add Submission modal

--- src/components/Organisms/AddEditSubmission/AddEditSubmission.js
+++ src/components/Organisms/AddEditSubmission/AddEditSubmission.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
-import { Image } from 'semantic-ui-react';
+import { Image, Header, Button } from 'semantic-ui-react';
 
 class AddEditSubmission extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			files: [],
-			featuredSubmission: {}
+			featuredSubmission: {},
+			featuredSubmissionIndex: 0
 		};
 	}
 
 	handleClick(e) {
-		console.log(e.target);
-		// e.target.classList.add('selected');
+		var submissionReference = e.target.parentNode;
+		var submissionsContainer = e.target.parentNode.parentNode;
+		var nodes = Array.prototype.slice.call(submissionsContainer.children);
+		var selectedIndex = nodes.indexOf(submissionReference);
+
+		this.setState({ featuredSubmissionIndex: selectedIndex });
+	}
+
+	handleSubmission() {
+		console.log('submitted');
 	}
 
 	onDrop(files) {
@@ -25,9 +34,17 @@ class AddEditSubmission extends Component {
 	render() {
 		return (
 			<div className="add-edit-submission-wrapper">
+				<header>
+					<Header as="h1">Submit Submissions</Header>
+					<p>
+						Upload your submission and select the photo you want to
+						be featured
+					</p>
+				</header>
 				<Dropzone
 					accept="image/jpeg, image/png"
 					onDrop={this.onDrop.bind(this)}
+					className="drop-container"
 				>
 					<p>
 						Try dropping some files here, or click to select files
@@ -35,15 +52,41 @@ class AddEditSubmission extends Component {
 					</p>
 				</Dropzone>
 				<div className="submission-results">
-					{this.state.files.map(f => (
-						<aside
-							key={f.name}
-							onClick={this.handleClick.bind(this)}
-						>
-							<Image size="small" rounded src={f.preview} />
-						</aside>
-					))}
+					<header>
+						{this.state.files.length > 0 ? (
+							<h3>Your submissions:</h3>
+						) : (
+							''
+						)}
+					</header>
+
+					<div className="submission-container">
+						{this.state.files.map((f, index) => {
+							let className;
+
+							if (index === this.state.featuredSubmissionIndex) {
+								className = 'featured';
+							}
+
+							return (
+								<aside
+									key={f.name}
+									onClick={this.handleClick.bind(this)}
+									className={className}
+								>
+									<Image
+										size="small"
+										rounded
+										src={f.preview}
+									/>
+								</aside>
+							);
+						})}
+					</div>
 				</div>
+				<Button onClick={this.handleSubmission.bind(this)}>
+					Upload Submission
+				</Button>
 			</div>
 		);
 	}
